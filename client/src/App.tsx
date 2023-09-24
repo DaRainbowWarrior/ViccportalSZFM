20a3f4e6e379ce94adf48c6ca4d8e6a4bc38b96b
rake <sahetes@rdisoftware.com>
2023-09-24 18:21:34 +0200

    client init pushed

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Timeline from './components/Timeline'
+import {Container} from '@mui/material'
+
+function App() {
+
+  return (
+    <>
+    <Container>
+      <Timeline/>
+    </Container>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/JokeCard.tsx
@@ -0,0 +1,43 @@
+import {useState} from 'react'
+import {Card, CardContent, Typography, IconButton, List, ListItem, Paper} from '@mui/material'
+import { Joke } from './Timeline'
+
+function JokeCard({title,author,description,dislikes,likes,tags}:Joke) {
+  const [isLiked, setIsLiked] = useState(false);
+  const [isDisliked, setIsDisliked] = useState(false);
+
+
+  return (
+    <Card sx={{ minWidth: 500, marginTop: 2 }}>
+      <CardContent>
+        <Typography variant='h5'>
+          {title}
+        </Typography>
+        <Typography sx={{fontSize: 11, color: 'gray'}}>
+          {author}
+        </Typography>
+        <Typography variant='body2'>
+          {description}
+        </Typography>
+
+      </CardContent>
+        <IconButton onClick={()=>setIsLiked(prev => !prev)}>
+          {isLiked ? likes+1 : likes }
+        </IconButton>
+        <IconButton onClick={()=>setIsDisliked(prev => !prev)}>
+          {isDisliked ? dislikes+1 : dislikes} 
+        </IconButton>
+    <List>
+      {tags.map((text, index) =>{
+        return (
+        <ListItem key={index}>
+          <Paper>{text}</Paper>
+        </ListItem>
+        )
+      })}
+    </List>
+    </Card>
+  )
+}
+
+export default JokeCard
